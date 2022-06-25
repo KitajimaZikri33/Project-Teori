a3e0ce6337231377ab2e3b44d1ce8313b27ec6cb
KitajimaZikri33 <108131729+KitajimaZikri33@users.noreply.github.com>
2022-06-25 15:58:21 +0700

    Add files via upload

---
+++ crud/menu.php
@@ -0,0 +1,10 @@
+<nav>
+	<ul>
+		<li><a href="index.php">FILM</a></li>
+		<li><a href="jadwal.php?menu=jadwal">JADWAL</a></li>
+		<li><a href="kursi.php?menu=kursi">KURSI</a></li>
+		<li><a href="operator.php?menu=operator">OPERATOR</a></li>
+		<li><a href="teater.php?menu=teater">TEATER</a></li>
+		<li><a href="transaksi.php?menu=transaksi">TRANSAKSI</a></li>
+	</ul>
+</nav>
